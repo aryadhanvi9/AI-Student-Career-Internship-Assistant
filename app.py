f4dc2656dff11238716544b3ab2e5e00e88fc248
aryadhanvi9 <aryadhanvi9@gmail.com>
2026-06-17 09:05:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("AI Based Student Career and Internship Assistant")
+from career_recommender import recommend_career
+from internship_recommender import recommend_internship
+
+skills = input("Enter your skills (comma separated): ")
+
+career = recommend_career(skills)
+internship = recommend_internship(skills)
+
+print("Recommended Career:", career)
+print("Recommended Internship:", internship)
