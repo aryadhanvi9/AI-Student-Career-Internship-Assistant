a7660586a872fcd5c82dee3864bc0fef33342fe6
aryadhanvi9 <aryadhanvi9@gmail.com>
2026-06-17 08:51:33 -0700

    Create career_recommender.py

---
+++ career_recommender.py
@@ -0,0 +1,7 @@
+def recommend_career(skills):
+    if "Python" in skills:
+        return "AI/ML Engineer"
+    elif "Java" in skills:
+        return "Software Developer"
+    else:
+        return "Explore different career paths"
