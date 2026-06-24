cb5a18c1115bc741f6a1b8a1ffab2432327093dc
aryadhanvi9 <aryadhanvi9@gmail.com>
2026-06-23 23:22:56 -0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from career_recommender import recommend_career
 from internship_recommender import recommend_internship
+import os
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -13,7 +14,8 @@ def home():
     return render_template(
         "index.html",
         career=career,
-     internship=internship
+      internship=internship
     )
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+ app.run(host="0.0.0.0", port=port)
